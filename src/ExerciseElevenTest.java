991a41fc606132f05eaa36224aee50e9cc183ac0
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-07-10 09:49:22 +0200

    add tests

--- src/ExerciseElevenTest.java
+++ src/ExerciseElevenTest.java
@@ -7,7 +7,14 @@ public class ExerciseElevenTest {
 
   @Test
   public void isAnagramTrue() throws Exception {
+    AnagramFinder anagramFinder = new AnagramFinder();
+    assertTrue(anagramFinder.isAnagram("I am a weakish speller", "William Shakespeare"));
+  }
 
+  @Test
+  public void isAnagramFalse() throws Exception {
+    AnagramFinder anagramFinder = new AnagramFinder();
+    assertFalse(anagramFinder.isAnagram("hello", "szia"));
   }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     AnagramFinder anagramFinder = new AnagramFinder();
     System.out.println(anagramFinder.isAnagram("I am a weakish speller", "William Shakespeare"));
     System.out.println(anagramFinder.isAnagram("Gyomorrontás", "toronymorgás"));
+    System.out.println(anagramFinder.isAnagram("hello", "szia"));
   }
 
 }
